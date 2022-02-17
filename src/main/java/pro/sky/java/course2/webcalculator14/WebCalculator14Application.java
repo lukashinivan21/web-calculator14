a841d72195ba31e883ea9738e180c0318e01d10b
Ivan <uvanes21@yandex.ru>
2022-02-17 09:37:00 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/java/course2/webcalculator14/WebCalculator14Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.webcalculator14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCalculator14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCalculator14Application.class, args);
+    }
+
+}
